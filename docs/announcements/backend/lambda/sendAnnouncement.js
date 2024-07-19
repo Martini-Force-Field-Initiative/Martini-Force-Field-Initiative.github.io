8178ffd5ce3e1e3556ae342dd00a02eb2523518d
Daniel Pastor Ramirez <daniel.ramirezecheme@ucalgary.ca>
2024-07-19 15:47:03 -0600

    Announcements backend fully functional.

--- docs/announcements/backend/lambda/sendAnnouncement.js
+++ docs/announcements/backend/lambda/sendAnnouncement.js
@@ -19,19 +19,29 @@ exports.handler = async (event) => {
       const data = await s3.getObject(params).promise();
       const announcementContent = data.Body.toString('utf-8');
 
+      // Extract the title and content without front matter from the markdown file
+      const { title, description } = extractTitleAndContent(announcementContent);
+
       const subscribers = await dynamoDb.scan({ TableName: tableName }).promise();
       const emailPromises = subscribers.Items.map(item => {
         const emailParams = {
           Source: 'daniel.ramirezecheme@ucalgary.ca',
           Destination: { ToAddresses: [item.email] },
           Message: {
-            Subject: { Data: `New Announcement: ${key}` },
+            Subject: { Data: `New Announcement from the Martini FF Initiative.` },
             Body: { Html: { Data: 
               `
-              ${announcementContent}
+              <h3>Title<hr></h3>
+              ${title}
               
+              <h3>Short description<hr></h3>
+              ${description}
+
+              <h3>Details<hr></h3>
+              For more details please visit our <a href="https://cgmartini.nl/docs/announcements/">Announcement Board</a>.
+                            
               <br><br>
-              
+
               <hr style="border: 0.5px solid #000;">
               If you no longer wish to receive emails from us, you can <a href="https://ilidpuzbe9.execute-api.ca-central-1.amazonaws.com/prod/unsubscribe?email=${encodeURIComponent(item.email)}&token=${item.token}">unsubscribe from our mailing list</a>.
               
@@ -52,3 +62,23 @@ exports.handler = async (event) => {
     }
   }
 };
+
+function extractTitleAndContent(markdownContent) {
+  const frontMatterMatch = markdownContent.match(/---([\s\S]*?)---/);
+  if (!frontMatterMatch) {
+    throw new Error('Invalid markdown format: Missing front matter');
+  }
+  const frontMatter = frontMatterMatch[1];
+  const titleMatch = frontMatter.match(/title: ["']?([\s\S]*?)["']?(?=\n\S|$)/);
+  if (!titleMatch) {
+    throw new Error('Invalid markdown format: Missing title');
+  }
+  const title = titleMatch[1].trim();
+  const descriptionMatch = frontMatter.match(/description:\s*(\|)?\s*([\s\S]*?)(?=\n\S|$)/);
+  if (!descriptionMatch) {
+    throw new Error('Invalid markdown format: Missing description');
+  }
+  const description = descriptionMatch[2].trim();
+
+  return { title, description };
+}
